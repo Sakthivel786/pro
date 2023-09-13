c31a2d818aefb4e7e0d1a95168fb468f5a46a1c6
Sakthivel786 <101980113+Sakthivel786@users.noreply.github.com>
2023-09-13 12:12:09 +0530

    Add files via upload

---
+++ A1.java
@@ -0,0 +1,22 @@
+package practise;
+
+public class A1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double []a=new double[3];
+		// to print the address of the array elements
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+		// to intialize the value of the array elements
+		a[0]=100;
+		a[1]=200;
+		a[2]=300;
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+
+	}
+
+}
